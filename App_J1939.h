83b2511c4a90c1b54cc42bb8ad766861b7b1d00b
Altamash Abdul Rahim <warlock.unleash@gmail.com>
2019-02-10 02:35:41 +0530

    Final edit

---
+++ App_J1939.h
@@ -0,0 +1,18 @@
+/*
+ * App_J1939.h
+ *
+ *  Created on: 09-Feb-2019
+ *      Author: Altamash Abdul Rahim
+ */
+
+#ifndef _APP_J1939_H_
+#define _APP_J1939_H_
+
+#include "main.h"
+#include "TP_J1939.h"
+
+
+void App_Init(void);
+void App_Process(void);
+
+#endif /* APP_J1939_H_ */
